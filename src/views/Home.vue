ece686026926e6faec1be1ef6f821308a0085fde
Noushedul1 <noushedakib@gmail.com>
2021-12-22 11:53:31 +0600

    add computed search box

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="home">
-    <p v-for="name in names" :key="name">
+    <input type="text" v-model="search">
+    <p>search term --- {{search}}</p>
+    <p v-for="name in matchingNames" :key="name">
       {{name}}
     </p>
   </div>
@@ -11,8 +13,12 @@ import {computed, ref} from 'vue';
 export default {
   name: 'Home',
   setup(){
+    const search = ref();
     const names = ref(['akib','aki','shaki','mohima','arman']);
-    return {names}
+    const matchingNames = computed(()=>{
+      return names.value.filter((name)=>name.includes(search.value));
+    });
+    return {names,search,matchingNames}
   }
 }
 </script>
